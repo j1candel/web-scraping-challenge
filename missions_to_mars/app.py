b50bfd0128b962b757d394cd1e30e09ea8b9a6f8
Jovany Candelario <jovany.candel@gmail.com>
2020-11-10 11:50:13 -0800

    html progress

--- missions_to_mars/app.py
+++ missions_to_mars/app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, redirect 
-from flask_pymongo import PyMongo 
-
+from flask_pymongo import PyMongo
+import scrape_mars
 
 app = Flask(__name__)
 
-app.config['PyMongo'] ='mongodb://localhost:27017'
-mongo = PyMongo(app)
+mongo = PyMongo(app, uri='mongodb://localhost:27017/mars_app')
 
 @app.route('/')
-def index
+def index():
+
+    mars_dict = mongo.db.mars_dict.find_one()
+
+    return render_template('index.html', mars = mars_dict)
+
+if __name__ == "__main__":
+    app.run(debug=True)
